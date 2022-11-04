27ff61eb05035b1129bfb968f4e3ee99f20b6e99
ikamrava <ikamrava>
2022-11-04 22:37:04 +0000

    without checkbox

---
+++ index.js
@@ -0,0 +1,137 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+generateBtn = document.querySelector(".generatorbtn");
+copyText1 = document.getElementById("pass1");
+copyText2 = document.getElementById("pass2");
+copybtn1 = document.querySelector(".copybtn1");
+copybtn2 = document.querySelector(".copybtn2");
+firstOutput = document.getElementById("pass1");
+secondOutput = document.getElementById("pass2");
+
+generatedPasswordOne = "";
+generatedPasswordTwo = "";
+
+function passGen(num, list) {
+  let finalPasword = "";
+  for (i = 0; i < num; i++) {
+    id = Math.floor(Math.random() * list.length);
+    let randomChar = list[id];
+    finalPasword += randomChar;
+  }
+  return finalPasword;
+}
+
+function copytext(dom) {
+  dom.select();
+  navigator.clipboard.writeText(dom.value);
+  alert("Copied the text: " + dom.value);
+}
+
+generateBtn.addEventListener("click", function () {
+  userInput = document.getElementById("userinput").value;
+
+  password1 = passGen(userInput, characters);
+  password2 = passGen(userInput, characters);
+  firstOutput.value = password1;
+  secondOutput.value = password2;
+});
+
+copybtn1.addEventListener("click", function () {
+  copytext(copyText1);
+});
+
+copybtn2.addEventListener("click", function () {
+  copytext(copyText2);
+});
